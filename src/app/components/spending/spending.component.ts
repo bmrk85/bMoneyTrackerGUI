20321e20e7f13753bc7f32b946bf2f45aa094019
Mark Banu <mark.banu@clarmont.hu>
2020-03-06 18:42:15 +0100

    spending dataTable + functions implemented

--- src/app/components/spending/spending.component.ts
+++ src/app/components/spending/spending.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {SpendingService} from '../../services/spending-service/spending.service';
+import {MatDialog} from '@angular/material/dialog';
+import {SpecificDateModalComponent} from '../../modals/specific-date-modal/specific-date-modal.component';
 
 @Component({
   selector: 'app-spending',
@@ -8,42 +10,56 @@ import {SpendingService} from '../../services/spending-service/spending.service'
 })
 export class SpendingComponent implements OnInit {
 
-  showAll = false;
-  showThisMonth = false;
-  showThisWeek = false;
+  @ViewChild('dataTable', {static: false})
+  dataTable: ElementRef;
 
-  constructor(private spendingService: SpendingService) {
+  spendings = [];
+  tableToggled = false;
+  displayedColumns: string[] = ['spendingId', 'spendingName', 'spendingCategory', 'spendingDate', 'spendingAmount'];
+
+  constructor(private spendingService: SpendingService,
+              public dialog: MatDialog) {
   }
 
   ngOnInit() {
   }
 
-  toggleAll() {
-    this.showThisWeek = false;
-    this.showThisMonth = false;
-    this.showAll = true;
 
-    this.spendingService.getAll().subscribe(data =>{
-      console.log(data);
-    })
-
-
-
-
+  getTotalCost() {
+    return this.spendings.map(s => s.amount).reduce((acc, value) => acc + value, 0);
   }
 
-  toggleThisMonth() {
-    this.showThisWeek = false;
-    this.showAll = false;
-    this.showThisMonth = true;
+  toggleTable(param: string) {
+    this.tableToggled = true;
+    if (param === 'all') {
+      this.spendingService.getAll().subscribe(data => {
+        console.log(data);
+        this.spendings = data;
+      })
+    } else if (param === 'week') {
+      let date = new Date();
+      this.spendingService.getBetweenDates(new Date(date.setDate(date.getDate() - 7)), new Date()).subscribe(data => {
+        console.log(data);
+        this.spendings = data;
+      })
+
+
+    } else if (param === 'month') {
+      let date = new Date();
+      this.spendingService.getBetweenDates(new Date(date.setDate(date.getMonth() - 1)), new Date()).subscribe(data => {
+        console.log(data);
+        this.spendings = data;
+      })
+    } else if (param === 'specificDate') {
+      const dialogRef = this.dialog.open(SpecificDateModalComponent);
+      dialogRef.afterClosed().subscribe(data =>
+        this.spendingService.getBetweenDates(data.dateFrom, data.dateTo).subscribe(data => {
+          console.log(data);
+          this.spendings = data;
+        })
+      )
+    }
+    this.dataTable.nativeElement.scrollIntoView();
+
   }
-
-  toggleThisWeek() {
-    this.showAll = false;
-    this.showThisMonth = false;
-    this.showThisWeek = true;
-
-  }
-
-
 }
--- src/app/services/spending-service/spending.service.ts
+++ src/app/services/spending-service/spending.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Spending} from '../../models/spending';
 
@@ -19,9 +19,19 @@ export class SpendingService {
         'Content-Type': 'application/json'
       })
     });
-
-
   }
 
+  getBetweenDates(dateFrom: Date, dateTo: Date): Observable<Spending[]> {
+
+    return this.http.get<Spending[]>(`http://localhost:8080/spendings/date`, {
+      params: {
+        dateFrom: dateFrom.toISOString(),
+        dateTo: dateTo.toISOString()
+      },
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
 
 }
